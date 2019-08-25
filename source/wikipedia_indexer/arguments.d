9e97fdae90482725d4d80fbe5c862ab2e24bda90
Johannes Loher <johannes.loher@fg4f.de>
2019-08-25 14:32:28 +0200

    print usage on commandline options parsing error

--- source/wikipedia_indexer/arguments.d
+++ source/wikipedia_indexer/arguments.d
@@ -2,38 +2,31 @@ module wikipedia_indexer.arguments;
 
 import core.stdc.stdlib : exit;
 import std.format : format;
-import std.getopt : config, defaultGetoptPrinter, getopt, GetOptException;
+import std.getopt : config, defaultGetoptPrinter, getopt, GetOptException, GetoptResult;
 import std.stdio : writeln;
 
 struct Arguments
 {
     string inputFileName;
     string outputFileName;
-    string executableName;
     size_t numberOfPages;
 }
 
 auto handleArguments(string[] args)
 {
-    string inputFileName;
-    string outputFileName;
     string executableName = args[0];
-    size_t numberOfPages;
 
     try
     {
-        auto helpInformation = getopt(args, config.required, "input|i",
-                "The name of the file to read the data from.", &inputFileName,
-                config.required, "output|o",
-                "The name of the file to write the index to.",
-                &outputFileName, "pages|p",
-                "The number of pages to process. If set to 0, all pages are processed.",
-                &numberOfPages);
+        auto helpInformationAndArguments = args.parseArguments;
 
-        if (helpInformation.helpWanted)
+        if (!helpInformationAndArguments.helpInformation.helpWanted)
         {
-            defaultGetoptPrinter(format!"Usage: %s [options] \n\nOptions:"(executableName),
-                    helpInformation.options);
+            return helpInformationAndArguments.arguments;
+        }
+        else
+        {
+            printHelp(executableName, helpInformationAndArguments.helpInformation);
             exit(0);
         }
     }
@@ -41,8 +34,36 @@ auto handleArguments(string[] args)
     {
         writeln("Skipping execution because there was an error while trying to parse the commandline options: ",
                 e.msg);
+        printHelp(executableName, [executableName, "-h"].parseArguments.helpInformation);
         exit(1);
     }
+    assert(0);
+}
 
-    return Arguments(inputFileName, outputFileName, executableName, numberOfPages);
+private:
+
+auto parseArguments(string[] args)
+{
+    import std.typecons : tuple;
+
+    string inputFileName;
+    string outputFileName;
+    size_t numberOfPages;
+
+    auto getoptResult = getopt(args, config.required, "input|i",
+            "The name of the file to read the data from.", &inputFileName,
+            config.required, "output|o",
+            "The name of the file to write the index to.",
+            &outputFileName, "pages|p",
+            "The number of pages to process. If set to 0, all pages are processed.",
+            &numberOfPages);
+
+    return tuple!("helpInformation", "arguments")(getoptResult,
+            Arguments(inputFileName, outputFileName, numberOfPages));
+}
+
+void printHelp(string executableName, GetoptResult getoptResult)
+{
+    defaultGetoptPrinter(format!"Usage: %s [options] \n\nOptions:"(executableName),
+            getoptResult.options);
 }
