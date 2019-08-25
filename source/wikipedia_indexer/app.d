76e394676aed1da3e922fd19abe3f87c7eedd43c
Johannes Loher <johannes.loher@fg4f.de>
2019-08-25 13:49:37 +0200

    only use the latest revision of a page and add a simple test

--- source/wikipedia_indexer/app.d
+++ source/wikipedia_indexer/app.d
@@ -6,17 +6,15 @@ import std.file;
 import std.stdio;
 import std.experimental.logger;
 import wikipedia_indexer.arguments;
+import wikipedia_indexer.mmfile;
 import wikipedia_indexer.parser;
 
 void main(string[] args)
 {
     auto arguments = handleArguments(args);
 
-    size_t start = 0;
-    size_t end = getSize(arguments.inputFileName);
-
     infof(`Parsing file "%s".`, arguments.inputFileName);
-    auto result = parse(arguments.inputFileName, start, end, arguments.numberOfPages);
+    auto result = MmFileRange(arguments.inputFileName).parse(arguments.numberOfPages);
 
     info(`Sorting the parsed data.`);
     auto sorted = result.byKeyValue.array.sort!((a, b) => a.key < b.key);
--- source/wikipedia_indexer/parser.d
+++ source/wikipedia_indexer/parser.d
@@ -3,33 +3,31 @@ module wikipedia_indexer.parser;
 import dxml.parser;
 import std.algorithm;
 import std.range;
+import std.traits;
 import std.typecons;
 import std.experimental.logger;
 import wikipedia_indexer.hashset;
-import wikipedia_indexer.mmfile;
 
-alias UsernameTitle = Tuple!(string, "username", string, "title");
-alias usernameTitle = tuple!("username", "title");
-
-auto parse(string inputFileName, size_t start, size_t end, size_t numberOfPages)
+auto parse(R)(R input, size_t numberOfPages)
+        if (isForwardRange!R && isSomeChar!(ElementType!R))
 {
-    HashSet!string[string] articlesPerAuthor;
+    alias stringType = immutable(ElementType!R)[];
 
-    auto parserRange = MmFileRange(inputFileName)[start .. end].parseXML
-        .splitter!(e => e.type == EntityType.elementStart && e.name.equal("page"))
-        .filter!(r => r.hasElement!"title")
-        .filter!(r => r.hasElement!"username")
-        .map!(r => usernameTitle(r.getElement!"username", r.getElement!"title"))
+    HashSet!stringType[stringType] articlesPerAuthor;
+
+    auto parserRange = input.parseXML.splitAtElement("page")
+        .map!(findTitleAndLatestRevisionForPage)
+        .filter!(titleAndRevision => titleAndRevision.revision.hasElement("username"))
+        .map!(titleAndRevision => tuple!("username",
+                "title")(titleAndRevision.revision.findElement("username"), titleAndRevision.title))
         .enumerate(1).tee!((e) {
             if (e.index % 1_000 == 0)
                 infof("Parsed pages: %s", e.index);
         });
-
     alias ParsedElementType = ElementType!(typeof(parserRange));
     scope merge = (ParsedElementType e) {
         articlesPerAuthor = articlesPerAuthor.merge(e.value);
     };
-
     if (numberOfPages > 0)
     {
         parserRange.take(numberOfPages).each!(merge);
@@ -42,24 +40,87 @@ auto parse(string inputFileName, size_t start, size_t end, size_t numberOfPages)
     return articlesPerAuthor;
 }
 
-bool hasElement(string element, R)(R r)
+///
+unittest
 {
-    return !r.find!(e => e.type == EntityType.elementStart && e.name.equal(element)).empty;
+    import fluent.asserts : should;
+    import std.utf : byCodeUnit;
+
+    // given
+    auto input = `<mediawiki>
+    <page>
+        <title>SomeTitle</title>
+        <revision>
+            <timestamp>2018-08-14T06:47:24Z</timestamp>
+            <contributor>
+                <username>SomeUserName</username>
+            </contributor>
+        </revision>
+        <revision>
+            <timestamp>2018-08-15T07:39:25Z</timestamp>
+            <contributor>
+                <username>SomeOtherUserName</username>
+            </contributor>
+        </revision>
+    </page>
+        <page>
+        <title>SomeOtherTitle</title>
+        <revision>
+            <timestamp>2019-07-23T15:42:12Z</timestamp>
+            <contributor>
+                <username>SomeUserName</username>
+            </contributor>
+        </revision>
+        <revision>
+            <timestamp>2019-06-15T08:21:13Z</timestamp>
+            <contributor>
+                <username>SomeThirsUserName</username>
+            </contributor>
+        </revision>
+    </page>
+</mediawiki>`.byCodeUnit;
+
+    // when
+    auto result = input.parse(0);
+
+    // then
+    result.byKey.should.containOnly(["SomeOtherUserName", "SomeUserName"]);
+    result["SomeOtherUserName"][].should.containOnly(["SomeTitle"]);
+    result["SomeUserName"][].should.containOnly(["SomeOtherTitle"]);
 }
 
-auto getElement(string element, R)(R r)
+bool hasElement(R)(R r, string element)
+{
+    return r.canFind!(e => e.type == EntityType.elementStart && e.name.equal(element));
+}
+
+auto findElement(R)(R r, string element)
 {
     return r.find!(e => e.type == EntityType.elementStart
             && e.name.equal(element)).drop(1).front.text.array;
 }
 
-auto merge(HashSet!string[string] a, UsernameTitle e)
+auto splitAtElement(R)(R r, string element)
 {
-    scope create = () => HashSet!string(e.title);
-    scope update = (scope ref HashSet!string titles) {
-        titles.add(e.title);
+    return r.splitter!(e => e.type == EntityType.elementStart && e.name.equal(element)).drop(1);
+}
+
+auto findTitleAndLatestRevisionForPage(R)(R page)
+{
+    immutable title = page.findElement("title");
+    auto revision = page.splitAtElement("revision")
+        .maxElement!(revision => revision.findElement("timestamp"));
+
+    return tuple!("title", "revision")(title, revision);
+}
+
+auto merge(T)(HashSet!T[T] existingEntries, Tuple!(T, "username", T, "title") newEntry)
+{
+    scope create = () => HashSet!T(newEntry.title);
+    scope update = (scope ref HashSet!T titles) {
+        titles.add(newEntry.title);
         return titles;
     };
-    a.update(e.username, create, update);
-    return a;
+    existingEntries.update(newEntry.username, create, update);
+    return existingEntries;
 }
